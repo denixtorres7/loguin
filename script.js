04aebc8ffc3addac21367b7a47c957f024ff55e2
denixtorres7 <44268208+denixtorres7@users.noreply.github.com>
2026-05-15 16:09:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,108 +1,25 @@
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
+const campoNombre = document.getElementById("nombre");
+const campoEmail = document.getElementById("email");
+const campoPassword = document.getElementById("password");
 
-*{
-margin:0;
-padding:0;
-box-sizing:border-box;
-}
+campoNombre.addEventListener("input", function(){
+    localStorage.setItem("nombreUsuario", campoNombre.value.trim());
+});
 
-html,body{
-width:948px;
-height:265px;
-overflow:hidden;
-background:transparent;
-font-family:'Poppins',sans-serif;
-}
+function login(){
 
-.pantalla{
-position:relative;
-width:948px;
-height:265px;
-background:transparent;
-}
+    const nombre = campoNombre.value.trim();
+    const email = campoEmail.value.trim();
+    const password = campoPassword.value.trim();
 
-/* NOMBRE EN FRANJA ROSA */
-.nombre{
-position:absolute;
-top:4px;
-left:230px;
-width:490px;
-height:55px;
-background:transparent;
-border:none;
-outline:none;
-font-family:'Poppins',sans-serif;
-font-size:24px;
-font-weight:700;
-text-align:center;
-color:white;
-caret-color:white;
-text-transform:uppercase;
-}
+    if(nombre !== "" && email !== "" && password !== ""){
 
-.nombre::placeholder{
-color:rgba(255,255,255,.75);
-}
+        localStorage.setItem("nombreUsuario", nombre);
 
-/* EMAIL SOLO DENTRO DEL ÓVALO */
-.email{
-position:absolute;
-top:105px;
-left:315px;
-width:500px;
-height:42px;
-background:transparent;
-border:none;
-outline:none;
-font-family:'Poppins',sans-serif;
-font-size:20px;
-font-weight:700;
-text-align:left;
-color:#111;
-caret-color:#111;
-padding-left:15px;
-}
+        document.getElementById("mensaje").innerHTML = "LOGIN CORRECTO";
 
-/* CONTRASEÑA SOLO DENTRO DEL ÓVALO */
-.password{
-position:absolute;
-top:178px;
-left:415px;
-width:400px;
-height:42px;
-background:transparent;
-border:none;
-outline:none;
-font-family:'Poppins',sans-serif;
-font-size:20px;
-font-weight:700;
-text-align:left;
-color:#111;
-caret-color:#111;
-padding-left:15px;
-}
+    }else{
 
-/* BOTÓN CHECK */
-.btn-login{
-position:absolute;
-top:100px;
-left:845px;
-width:90px;
-height:90px;
-background:transparent;
-border:none;
-cursor:pointer;
-}
-
-#mensaje{
-position:absolute;
-bottom:0;
-left:0;
-width:948px;
-height:20px;
-font-family:'Poppins',sans-serif;
-font-size:14px;
-font-weight:700;
-text-align:center;
-color:#ff00ff;
+        document.getElementById("mensaje").innerHTML = "COMPLETE LOS CAMPOS";
+    }
 }
