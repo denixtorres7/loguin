d840aca63fc3ae65880fabbf013fde122fae7b36
denixtorres7 <44268208+denixtorres7@users.noreply.github.com>
2026-05-15 11:22:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,138 +1,50 @@
-const usuario = {
-
-    nombre: "DENIZE",
-    email: "admin@test.com",
-    password: "1234"
-};
-
 function login(){
 
-    const nombre = document.getElementById("nombre").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+const nombre=
+document.getElementById("nombre").value;
 
-    if(
+const email=
+document.getElementById("email").value;
 
-        nombre.toUpperCase() === usuario.nombre &&
-        email === usuario.email &&
-        password === usuario.password
+const password=
+document.getElementById("password").value;
 
-    ){
 
-        // GUARDAR NOMBRE
-        localStorage.setItem(
-            "nombreUsuario",
-            nombre
-        );
+if(
 
-        document.getElementById("mensaje").innerHTML =
-        "LOGIN CORRECTO";
+nombre!="" &&
+email!="" &&
+password!=""
 
-        // SI QUIERES CAMBIAR DE PÁGINA
-        // window.location.href="home.html";
+){
 
-    }else{
+localStorage.setItem(
+"nombreUsuario",
+nombre
+);
 
-        document.getElementById("mensaje").innerHTML =
-        "DATOS INCORRECTOS";
-    }
-/* =====================
-PLACEHOLDER GRIS
-===================== */
 
-.nombre::placeholder,
-.email::placeholder,
-.password::placeholder{
+document.getElementById(
+"mensaje"
+).innerHTML=
 
-color:rgba(120,120,120,.8);
+"LOGIN CORRECTO";
 
-font-weight:700;
+
+/* OPCIONAL REDIRECCION */
+
+// window.location.href="home.html";
 
 }
 
+else{
 
-/* =====================
-GUIAS TEMPORALES
-===================== */
+document.getElementById(
+"mensaje"
+).innerHTML=
 
-.guia{
-
-position:absolute;
-
-background:red;
-
-color:white;
-
-padding:6px 10px;
-
-font-size:16px;
-
-font-weight:bold;
-
-border-radius:10px;
-
-z-index:9999;
-
-box-shadow:0 0 12px red;
+"COMPLETE LOS CAMPOS";
 
 }
 
-
-/* NOMBRE */
-
-.guiaNombre{
-
-top:18%;
-left:50%;
-
-transform:translateX(-50%);
-
-}
-
-
-/* EMAIL */
-
-.guiaEmail{
-
-top:40%;
-left:58%;
-
-}
-
-
-/* PASSWORD */
-
-.guiaPassword{
-
-top:49%;
-left:58%;
-
-}
-
-
-/* BORDES TEMPORALES */
-
-.nombre{
-
-border:2px dashed red!important;
-
-}
-
-.email{
-
-border:2px dashed blue!important;
-
-}
-
-.password{
-
-border:2px dashed lime!important;
-
-}
-
-.btn-login{
-
-border:3px solid magenta!important;
-
-}
 }
