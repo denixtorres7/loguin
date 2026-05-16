3ce8c8b07d49c93d4e9c81eb06eba5dbc73591c2
denixtorres7 <44268208+denixtorres7@users.noreply.github.com>
2026-05-15 21:12:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ document.getElementById("email").value;
 const password=
 document.getElementById("password").value;
 
-
 if(
 
 nombre!="" &&
@@ -17,6 +16,8 @@ email!="" &&
 password!=""
 
 ){
+
+/* guarda el nombre */
 
 localStorage.setItem(
 "nombreUsuario",
@@ -28,11 +29,6 @@ document.getElementById(
 ).innerHTML=
 "LOGIN CORRECTO";
 
-  setTimeout(()=>{
-
-window.location.href="nombre.html";
-
-},200);
 }
 
 else{
